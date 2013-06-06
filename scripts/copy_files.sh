287009f717ff782349805118d19d378fd593353f
Drew Hendrickson <dhendrickson@outlook.com>
2013-06-06 18:43:37 +0930

    no output from cp needed

--- scripts/copy_files.sh
+++ scripts/copy_files.sh
@@ -3,6 +3,7 @@ COUNTER=1
 while read p; do
 #  echo "========"
 #  echo "$p"
-  cp -i "$p" $2/$COUNTER.$(basename $p)
+  cp "$p" "$2/$COUNTER.$(basename "$p")"
   let COUNTER=COUNTER+1 
+  echo $(basename "$p")
 done < $1
